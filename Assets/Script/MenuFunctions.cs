eda0517f14b487aa470b9d29649f83ef3a2c0cdd
Andrea Forti <andry.foffos@libero.it>
2023-07-04 19:41:47 +0200

    Add Menu Scene

---
+++ Assets/Script/MenuFunctions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctions : MonoBehaviour
+{
+
+	[SerializeField] private TextMeshProUGUI loading;
+	public void Play()
+	{
+		loading.gameObject.SetActive(true);
+		StartLoadGameScene();
+	}
+
+
+	void StartLoadGameScene()
+	{
+		StartCoroutine(LoadGameSceneAsync());
+	}
+
+	IEnumerator LoadGameSceneAsync()
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("WorldGen2", LoadSceneMode.Single);
+		yield return asyncLoad.isDone;
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName("WorldGen2"));
+	}
+}
